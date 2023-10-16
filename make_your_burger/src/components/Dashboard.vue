8b122cd1309b99f9daafc5200ed2a72984878a0b
José Vitor <josevitorpignataro@gmail.com>
2023-10-16 20:34:20 -0300

    Criando a tela de pedidos #28

--- make_your_burger/src/components/Dashboard.vue
+++ make_your_burger/src/components/Dashboard.vue
@@ -1,5 +1,36 @@
 <template>
-    <h1>Componente de dashboard</h1>
+    <div id="burger-table">
+        <div>
+            <div id="burger-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente:</div>
+                <div>Pão:</div>
+                <div>Carne:</div>
+                <div>Opcionais:</div>
+                <div>Ações:</div>
+            </div>
+            <div id="burger-table-rows">
+                <div class="burger-table-row">
+                    <div class="order-number">1</div>
+                    <div>João</div>
+                    <div>De trigo</div>
+                    <div>Maminha</div>
+                    <div>
+                        <ul>
+                            <li>Salame</li>
+                            <li>Tomate</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <select name="status" class="status">
+                            <option value="">Selecione</option>
+                        </select>
+                        <button class="delete-btn">Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,3 +43,64 @@ export default {
     },
 }
 </script>
+
+
+
+<style scoped>
+#burger-table{
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row{
+    display: flex;
+    flex-wrap: wrap;
+}
+
+
+#burger-table-heading{
+    font-weight: bold;
+    padding: 12px;
+    border-bottom: 3px solid #333;
+}
+
+#burger-table-heading div,
+.burger-table-row div{
+    width: 19%;
+}
+
+.burger-table-row {
+    width: 100%;
+    padding: 12px;
+    border-bottom: 1px solid #ccc;
+}
+
+#burger-table-heading .order-id,
+.burger-table-row .order-number{
+    width: 5%;
+}
+
+select{
+    padding: 12px 6px;
+    margin-right: 12px;
+}
+
+.delete-btn{
+    background: #222;
+    color: #FCBA03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: 0.5s;
+}
+
+.delete-btn:hover{
+    background: transparent;
+    color: #222;
+}
+
+</style>
